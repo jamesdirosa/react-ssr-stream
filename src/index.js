aff12761e83e118856b5e2cdbb705697ffff9e02
James DiRosa <james.dirosa@shopify.com>
2022-10-02 00:22:33 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import * as React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { App } from "./App";
 
-// const container = document.getElementById("root");
 hydrateRoot(document, <App assets={window.assetManifest} />);
