10da07a634a17b4c50910eaa51aaf172620c5761
James DiRosa <james.dirosa@shopify.com>
2023-03-01 15:31:23 +0000

    content

--- server/dal/mock-data/index.js
+++ server/dal/mock-data/index.js
@@ -1,36 +1,36 @@
 export const reviews = [
   {
-    name: "Jerry Seinfeld",
-    title: "The best coffee ever!",
+    name: 'Jerry Seinfeld',
+    title: 'The best coffee ever!',
     starCount: 5,
-    date: "April 9th, 2020",
-    review: "These pretzels are making me thirsty!",
+    date: 'April 9th, 2020',
+    review: 'These pretzels are making me thirsty!',
   },
   {
-    name: "Jean-Luc Picard",
-    title: "Seize this coffee!",
+    name: 'Jean-Luc Picard',
+    title: 'Seize this coffee!',
     starCount: 5,
-    date: "July 13, 2335",
-    review: "Make it so!",
+    date: 'July 13, 2335',
+    review: 'Make it so!',
   },
   {
-    name: "Homer Simpson",
-    title: "Mmmmm",
+    name: 'Homer Simpson',
+    title: 'Mmmmm',
     starCount: 5,
-    date: "October 30th, 2009",
-    review: "Goes great with donuts. :drool:",
+    date: 'October 30th, 2009',
+    review: 'Goes great with donuts. :drool:',
   },
 ];
 
 export const profile = {
-  url: "/avatar.jpeg",
-  name: "James DiRosa",
+  url: '/avatar.jpeg',
+  name: 'James DiRosa',
 };
 
 export const content = {
-  title: "Premium Blend",
+  title: 'Premium Blend',
   description:
-    "Our most popular coffee. Notes of beans and whatever else make coffee taste good. Grown on the tallest coffee trees, and harvested by coffee elves at the peak of freshness. Price is for 16oz",
+    'Our most popular coffee. Notes of beans and whatever else make coffee taste good. Grown on the tallest coffee trees, and harvested at the peak of freshness. Price is for 16oz',
   price: 34.21,
-  img: "/coffee.jpeg",
+  img: '/coffee.jpeg',
 };
